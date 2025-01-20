242aea6793ebef350fa8c51917b813c660de18b2
erikamc99 <erikamc99@gmail.com>
2025-01-20 17:27:37 +0100

    fix some styles in header and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 function Header() {
   return (
-    <nav className="absolute w-full py-6 bg-blue-500">
-      <ul className="flex items-center justify-around max-w-6xl mx-auto">
+    <nav className="absolute w-full py-6 bg-blue-500 px-4">
+      <ul className="flex items-center justify-between max-w-6xl mx-auto">
         <li>
           <h1 className="text-white text-2xl font-bold">Cities in the World</h1>
         </li>
         <li className="flex space-x-6">
-          <a href="../index.html" className="text-white hover:underline">Home</a>
-          <a href="#" className="text-white hover:underline">About</a>
-          <a href="mailto:example@mail.com" className="text-white hover:underline">Contact</a>
+          <p className="text-white hover:underline">Home</p>
+          <p className="text-white hover:underline">About</p>
+          <p className="text-white hover:underline">Contact</p>
         </li>
       </ul>
     </nav>
--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -1,6 +1,6 @@
 function Restaurants() {
     return (
-        <div className="flex justify-center items-center space-x-4 bg-blue-100 mt-6">
+        <div className="flex justify-center items-center space-x-4 bg-blue-100 mt-6 py-2">
             <div className="text-center">
                 <img src="../src/assets/img/restaurante-disfrutar.jpg" alt="" className="rounded-full w-1/3 mx-auto"/>
                 <p>Disfrutar (Barcelona, España)</p>
