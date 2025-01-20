bdab6bcacbce722b9f3a439952bfce79badc2b63
erikamc99 <erikamc99@gmail.com>
2025-01-20 17:05:57 +0100

    footer styles, less superior line

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Footer from './components/Footer.jsx';
 import HeadCity from './components/HeadCity.jsx';
 import PopCities from './components/PopCities.jsx';
 import Restaurants from "./components/Restaurants.jsx";
-import SideRank from "./components/SideRank.jsx";
+
 function App() {
     return (
         <div>
             <Header />
             <HeadCity />
             <Restaurants />
-            
+            <Footer />
         </div>
     );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,18 @@
 function Footer() {
     return (
-        <div className="w-full bg-blue-500">
-          <div className="flex flex-wrap text-center text-white">
-            <div className="w-full md:w-1/3 p-5 border-r border-blue-800 md:text-left">
-              <p className="p-3 text-gray-400">
-                @2021 Company, Inc
-              </p>
+        <div className="w-full m-6">
+          <div className="flex flex-wrap text-left justify-around items-center">
+            <div className="text-left">
+              <p className="p-3 text-black">@2021 Company, Inc</p>
             </div>
-            <div className="w-full md:w-1/3 p-5 border-r border-blue-800">
-              <a href=""><img src="" alt="" /></a>
-              <a href=""><img src="" alt="" /></a>
-              <a href=""><img src="" alt="" /></a>
+            <div className="flex">
+              <img src="../src/assets/img/icon1.png" alt="" className="footer-icon"/>
+              <img src="../src/assets/img/icon2.png" alt="" className="footer-icon"/>
+              <img src="../src/assets/img/icon3.png" alt="" className="footer-icon"/>
             </div> 
           </div>
         </div>
     );
 };
+
 export default Footer;
